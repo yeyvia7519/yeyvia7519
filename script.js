c702c7d2e08af9cc0ce1834581a197ce6987db63
yeyvia7519 <114585771+yeyvia7519@users.noreply.github.com>
2022-09-28 08:57:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Yeyvi"; // update the part BETWEEN the "quotes"
+let userName = "yeyvia7519"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
